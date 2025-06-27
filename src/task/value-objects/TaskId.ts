485965541b17e736b52750fbb25adf29bdec62e1
H4KB <akabaharuka@gmail.com>
2025-06-28 02:37:54 +0900

    create task id

---
+++ src/task/value-objects/TaskId.ts
@@ -0,0 +1,16 @@
+import { nanoid } from "nanoid";
+import { z } from "zod";
+
+export const TaskIdSchema = z.object({
+  id: z.string().nanoid().brand<"TaskId">(),
+});
+
+export type TaskId = z.infer<typeof TaskIdSchema>;
+
+export const isTaskId = (value: unknown): value is TaskId => {
+  return TaskIdSchema.safeParse(value).success;
+};
+
+export const createTaskId = (): TaskId => {
+  return TaskIdSchema.parse({ id: nanoid() });
+};

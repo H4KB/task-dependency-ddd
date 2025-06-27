fcc09364de4cafb02cd7d50973eaef872d320a3e
H4KB <akabaharuka@gmail.com>
2025-06-28 02:46:11 +0900

    create task point

---
+++ src/task/value-objects/TaskPoint.ts
@@ -0,0 +1,18 @@
+import { z } from "zod";
+
+const TaskPointSchema = z
+  .object({
+    x: z.number().int().min(0), // UNIX 時間
+    y: z.number().int().min(1), // 行番号
+  })
+  .brand<"TaskPoint">();
+
+export type TaskPoint = z.infer<typeof TaskPointSchema>;
+
+export const isTaskPoint = (value: unknown): value is TaskPoint => {
+  return TaskPointSchema.safeParse(value).success;
+};
+
+export const createTaskPoint = (x: number, y: number): TaskPoint => {
+  return TaskPointSchema.parse({ x, y });
+};
